3e0b823da79b0fd95caff2cf151ef4e0530c482e
Zidane Abrisam Latif <zidaneabrisam1428@gmail.com>
2024-01-22 15:07:37 +0700

    Tailwind bro

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      // Membuat custom Spacing
       spacing:{
         '13': '3.25rem'
       }
     },
+    fontFamily:{
+      'Poppins' : ['Poppins']
+    }
   },
   plugins: [],
 }
